135930bb399eba8d068993eb8eaf6dc5cbf09680
Itiel Dario <itiel.dario@gmail.com>
2022-10-05 17:37:57 -0300

    Criação de classes, métodos e propriedades

---
+++ ex_09_classes_propriedades_metedos/index.php
@@ -0,0 +1,122 @@
+<?php
+$limpa = '<br>';
+$linha = '<hr>';
+
+# CLASSES - INTRODUÇÃO À (OPP) - PROGRAMAÇÃO ORIENTADA A OBJETOS
+
+# UMA CLASSE É UM MOLDE/FORMA/MODELO A PARTIR DO QUAL CRIAMOS UM OBJETO.
+# EXEMPLO: A CLASSE 'HUMANO' É UM MOL=DELO PARA, A PARTIR DELA, CRIARMOS UM CONJUNTO DE HOMENS E MULHERES,
+# CADA UMPARTILHANDO O MESMO 'MOLDE' MAS COM PROPRIEDADES E FUNÇÕES COM VALORES DIFERENTES.
+# HOMEM E MULHERES TEM AMBOAS CABELO, MAS O HOMEM PODE TER CABELO ESCURO E A MULHER CABELO CLARO
+
+/* CLASS HUMANO:
+    -> CABELO
+    -> GENERO
+    -> PESO
+    -> CAMINHAR
+*/
+
+/* HOMEM -> HUMANO
+    -> CABELO - CASTANHO
+    -> GENERO - MASCULINO
+    -> PESO - 75
+    -> CAMINHAR()
+*/
+
+/* MULHER -> HUMANO
+    -> CABELO - CLARO
+    -> GENERO - FEMININO
+    -> PESO - 63
+    -> CAMINHAR()
+    -> CANTAR() - PODE TER FUNÇÕES ADICIONAIS
+*/
+
+
+# AS CLASSES SAO DEFINICADAS PELO SEU NOME E POR UM BLOCO DE CÓDIGO QUE CONÉM AS SUAS PROPRIEDADES E MÉTODOS
+# O NOME DEVE SER SEMPRE ATRIBUIDO NA FORMA DE StudlyCaps/PascalCase/MixedCase
+
+class Humano
+{
+    // PROPRIEDADES E MÉTODOS
+}
+
+class JogadorFutebol
+{
+    // PROPRIEDADES E MÉTODOS
+}
+
+
+# O CORPO DE UMA CLASSE CONTÉM PROPRIEDADES E MÉTODOS 
+# PROPRIEDADES -> SÃO VARIÁVEIS QUE GUARDAM AS CARACTERÍSTICAS DO OBJETO.
+# MÉTODOS -> SÃO FUNÇÕES QUE DEFINEM O QUE O OBJETO PODE FAZER.
+
+# AS PROPRIEDADES TAMBÉM SÃO CONHECIDAS COMO FIELDS (CAMPOS) OU ATRIBUTOS DE UMA CLASSE
+# AS PROPRIEDADES TÊM QUE TER UM NÍVEL DE ACESSO ESPECÍFICADO
+
+class FiguraGeometria
+{
+    public $largura, $altura;   // PROPRIEDADES DE TODAS AS FIGURAS GOEMETRICAS
+    public $x;                  // PROPRIEDADES DE TODAS AS FIGURAS GOEMETRICAS
+    public $y;                  // PROPRIEDADES DE TODAS AS FIGURAS GOEMETRICAS
+
+    function novaArea($a, $b)   // MÉTODOS DE TODAS AS FIGURAS GOEMETRICAS
+    {
+        return $a * $b;
+    }
+}
+
+
+# PARA ACENDER ÀS PROPRIEDADES DE UMA CLASSE, DENTRO DOS MÉTODOS DA CLASSE -----------------------------------------------
+# É USADA A PSEUDO VARIÁVEL $this SEGUIDA DO OPERADOR SETA ->
+
+class SerHumano
+{
+    public $nome = 'João';            // PROPRIEDADE
+    public $sobrenome = 'Ribeiro';    // PROPRIEDADE
+
+    public function nomeCompleto()    // MÉTODO
+    {
+        $nome = 'Ana';                // VARIÁVEL COMUM (NÃO É UMA PROPRIEDADE)
+
+        return $this->nome . ' ' . $this->sobrenome;    // JUNTADO DUAS PROPRIEDADES
+    }
+}
+
+
+# AS CLASSAS RECORREM À UTILIZAÇÃO DE ACCESS MODIFIERS (NÍVEIS DE ACESSO)
+# OS NÍVEIS DE ACESSO AS DADOS INDICAM SE PODEMOS VER OS DADOS APENAS DENTRO DA CLASSE, SE PODEMOS VER OS DADOS FORA DA 
+# CLASSE OU SE ESTÃO PROTEGIDOS POR ALGUM MOTIVO
+
+
+
+# INSTANCIAR UM OBJETO 
+# UM OBJETO É UMA VARIÁVEL CRIADA A PARTIR DE UMA CLASSE.
+# INSTANCIAR UM OBJETO É CRIAR UM OBJETO A PARTIR DE UMA CLASSA ATRIBUINDO À VARIÁVEL A EXPRESSÃO NEW E O NOME DA CLASSE
+
+$homem = new SerHumano();   // NOVA CÓPIA DE SER HUMANO
+
+
+
+# COM A IMPLEMENTAÇÃO ANTERIOR PODEMOS AGORA ACEDER ÀS PROPRIEDADES E MÉTODOS DA CLASSE SerHumano
+
+echo $homem->nomeCompleto() . $linha;   // João Ribeiro
+
+
+
+# JUNTANDO TUDO 
+
+class Humano1
+{
+    public $nome = 'João';
+    public $sobrenome = 'Ribeiro';
+
+    public function nome_completo()
+    {
+        return $this->nome . ' ' . $this->sobrenome;
+    }
+}
+
+$homem = new Humano1;
+echo $homem->nome_completo() . $linha;  // João Ribeiro
+
+?>
